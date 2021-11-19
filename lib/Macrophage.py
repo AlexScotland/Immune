d4ceea9c8e5603d1f6263f78e5c1d78a16b8e814
Alex <1638309+AlexScotland@users.noreply.github.com>
2021-11-19 15:44:58 -0500

    added logic to better hunt

--- lib/Macrophage.py
+++ lib/Macrophage.py
@@ -8,7 +8,7 @@ class Macrophage(Cell):
 
     def __init__(self, x, y, color=arcade.color.AFRICAN_VIOLET, multiplier = 1, size=20):
         super().__init__(x, y, color, multiplier, size)
-        self.vision = self.size + 20
+        self.vision = self.size + 40
         self.speed = .5 * multiplier
         self.hunting = False
     
@@ -24,7 +24,7 @@ class Macrophage(Cell):
             cell = cell_list[cell_index]
             if Macrophage in cell.__class__.__mro__ or CivilianCell in cell.__class__.__mro__:
                 continue
-            if collision_detection(self.x + self.vision, self.y + self.vision, cell.x, cell.y, self.vision, cell.size):
+            if collision_detection(self.x, self.y, cell.x, cell.y, self.vision, cell.size):
                 # collision_detection(self.x + vision, self.y + vision, cell.x, cell.y, self.vision, cell.size):
                 self.hunting = True
                 if self.hunt_down_cell(cell):
@@ -35,7 +35,7 @@ class Macrophage(Cell):
         return False
     
     def hunt_down_cell(self, enemy_cell):
-        if not collision_detection(self.x+10, self.y+10, enemy_cell.x, enemy_cell.y, self.size, enemy_cell.size):
+        if collision_detection(self.x+10, self.y+10, enemy_cell.x, enemy_cell.y, self.vision, enemy_cell.size):
             if self.x > enemy_cell.x:
                 self.x -= self.speed
             else:
@@ -44,6 +44,6 @@ class Macrophage(Cell):
                 self.y -= self.speed
             else:
                 self.y += self.speed
-        else:
+        if collision_detection(self.x+10, self.y+10, enemy_cell.x, enemy_cell.y, self.size, enemy_cell.size):
             return True
     
